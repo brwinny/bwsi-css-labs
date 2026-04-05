2c0f9bd21d239674bc6d1c64f239d3f05030403b
Brynn <penguinbrynn@gmail.com>
2026-04-04 18:25:54 -0700

    Update lab_1a.py

--- labs/lab_1/lab_1a.py
+++ labs/lab_1/lab_1a.py
@@ -11,6 +11,7 @@ def main():
     name = "Brynn Frei" # TODO: Insert your name between the double quotes
 
     print(f"{name}, Welcome to the CSS course!")
+    print("I like to eat cookies!")
 
 if __name__ == "__main__":
     main()
